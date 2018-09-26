6aef4d1cdb1d9757215f3ab3c8511b55ae5212bb
Alwin James <38123613+jamealwi2@users.noreply.github.com>
2018-09-26 15:06:45 -0700

    Update mysparkexecutorlogger.py

--- mysparkexecutorlogger.py
+++ mysparkexecutorlogger.py
@@ -2,10 +2,17 @@ import logging
 import sys
 import os
 
+# Initializing the logger
 logger = logging.getLogger(__name__)
+
+# Setting the log level to 'DEBUG'
 logger.setLevel(logging.DEBUG)
+
+# Setting the executor log file location 
 handler = logging.FileHandler('/spark/jobs/executor.log')
 handler.setLevel(logging.DEBUG)
+
+# Setting the log format
 formatter = logging.Formatter('%(asctime)s -' ' %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
